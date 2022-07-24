db75f1a5c31271f09d02da49c203511fd2218115
andy94077 <andy94077@gmail.com>
2022-07-24 20:52:29 +0800

    Init of depth refactor

--- projects/mmdet3d_plugin/models/utils/depth_utils.py
+++ projects/mmdet3d_plugin/models/utils/depth_utils.py
@@ -1,5 +1,6 @@
 import math
 import torch
+from torch.nn import functional as F
 
 
 def bin_depths(depth_map, mode="LID", depth_min=1e-3, depth_max=60, num_depth_bins=80, target=False):
@@ -16,11 +17,13 @@ def bin_depths(depth_map, mode="LID", depth_min=1e-3, depth_max=60, num_depth_bi
         num_depth_bins [int]: Number of depth bins
         target [bool]: Whether the depth bins indices will be used for a target tensor in loss comparison
     Returns:
-        indices [torch.Tensor(H, W)]: Depth bin indices
+        If target is true, return the indices. Otherwise, return the logits.
+        * indices [torch.Tensor(*, H, W)]: Depth bin indices with dtype torch.long.
+        * logits [torch.Tensor(*, H, W, num_depth_bins+1)]: Depth bin logits with dtype torch.float.
     """
     if mode == "UD":
         bin_size = (depth_max - depth_min) / num_depth_bins
-        indices = ((depth_map - depth_min) / bin_size)
+        indices = (depth_map - depth_min) / bin_size
     elif mode == "LID":
         bin_size = 2 * (depth_max - depth_min) / (num_depth_bins * (1 + num_depth_bins))
         indices = -0.5 + 0.5 * torch.sqrt(1 + 8 * (depth_map - depth_min) / bin_size)
@@ -30,12 +33,13 @@ def bin_depths(depth_map, mode="LID", depth_min=1e-3, depth_max=60, num_depth_bi
     else:
         raise NotImplementedError
 
+    # Remove indicies outside of bounds
+    mask = (indices < 0) | (indices > num_depth_bins) | (~torch.isfinite(indices))
+    indices[mask] = num_depth_bins
     if target:
-        # Remove indicies outside of bounds
-        mask = (indices < 0) | (indices > num_depth_bins) | (~torch.isfinite(indices))
-        indices[mask] = num_depth_bins
-
         # Convert to integer
         indices = indices.type(torch.long)
-
-    return indices
+        return indices
+    # [*, H, W, num_depth_bins + 1]
+    logits = F.one_hot(indices, num_classes=num_depth_bins + 1).float()
+    return logits
